61ae37959c0eec079e0646086e7df2d74026840a
stevemann <steve.mann@technifybiz.com>
2021-09-05 13:47:42 +0530

    Print Positions

--- src/main/java/in/stevemann/tictactoe/services/BoardService.java
+++ src/main/java/in/stevemann/tictactoe/services/BoardService.java
@@ -13,6 +13,33 @@ import java.util.List;
 public class BoardService {
     private final MoveService moveService;
 
+    public void printPositions(Board board) {
+        int gridSize = board.getGame().getGridType().getSize();
+        for(int rowNumber = 0; rowNumber < gridSize * 2 + 1; rowNumber++) {
+            printRow(gridSize, rowNumber);
+        }
+    }
+
+    private void printRow(int gridSize, int row) {
+        int colStartNumber = (row / 2) * gridSize + 1;
+        if (row % 2 == 0) {
+            for (int col = 0; col < gridSize; col++) {
+                System.out.print(" + ");
+                System.out.print("-");
+            }
+        } else {
+            for (int j = colStartNumber; j < colStartNumber+ gridSize; j++) {
+                System.out.print(" | ");
+                System.out.print(j);
+            }
+        }
+        if (row % 2 == 0) {
+            System.out.println(" +");
+        } else {
+            System.out.println(" |");
+        }
+    }
+
     public Board newBoard(Game game) {
         return new Board(game);
     }
